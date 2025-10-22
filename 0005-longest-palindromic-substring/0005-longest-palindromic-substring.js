f3fe05e10186ccea2f490f88d7c1d6722cd81318
jumana2025 <jumana.edv@gmail.com>
2025-10-22 08:50:46 +0530

    Time: 22 ms (57.17%), Space: 55.8 MB (83.07%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    
+    if (s.length < 2) return s;
+
+    let start = 0, maxLen = 1;
+
+    function expandAroundCenter(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            if (right - left + 1 > maxLen) {
+                start = left;
+                maxLen = right - left + 1;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+   
+        expandAroundCenter(i - 1, i + 1);
+       
+        expandAroundCenter(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLen);
+}
+console.log(longestPalindrome("babad")); 
+console.log(longestPalindrome("cbbd")); 
